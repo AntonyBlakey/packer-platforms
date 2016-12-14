730879b45a99b00e76d0481e0a1a7d0540e5dedc
berney <berne.campbell@gmail.com>
2016-12-15 09:52:37 +1100

    Fix PACKER-LOCAL var names to be consistent

--- osx/template-setup-miscellaneous-ui.sh
+++ osx/template-setup-miscellaneous-ui.sh
@@ -3,9 +3,9 @@
 systemsetup -settimezone "PACKER-OSX-TIMEZONE"
 systemsetup -setsleep "Never"
 
-sudo -u vagrant defaults write NSGlobalDomain AppleLanguages -array 'PACKER-LOCALE-USING-HYPHEN' 'en'
-sudo -u vagrant defaults write NSGlobalDomain AppleLocale 'PACKER-LOCALE-USING-UNDERSCORE'
-sudo -u vagrant defaults write NSGlobalDomain NSPreferredSpellServerLanguage 'PACKER-LOCALE-USING-UNDERSCORE'
+sudo -u vagrant defaults write NSGlobalDomain AppleLanguages -array 'PACKER-LOCALE-WITH-HYPHEN' 'en'
+sudo -u vagrant defaults write NSGlobalDomain AppleLocale 'PACKER-LOCALE-WITH-UNDERSCORE'
+sudo -u vagrant defaults write NSGlobalDomain NSPreferredSpellServerLanguage 'PACKER-LOCALE-WITH-UNDERSCORE'
 
 sudo -u vagrant defaults  write com.apple.menuextra.clock DateFormat 'PACKER-OSX-DATEFORMAT'
 sudo -u vagrant defaults write NSGlobalDomain AppleICUForce24HourTime -boolean 'true'
